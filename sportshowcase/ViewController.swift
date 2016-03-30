622f73e6f5e4f8c128e4de7c30385df3b24afc3e
Jesse Tellez <jessetellez@jesses-macbook-pro-2.local>
2016-03-30 16:24:41 -0600

    Initial Commit

---
+++ sportshowcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sportshowcase
+//
+//  Created by Jesse Tellez on 3/30/16.
+//  Copyright © 2016 SunCat Developers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
